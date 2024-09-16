620d4b7f8751257a39170d570c4f258b9bdd268e
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-09-16 11:53:41 +0700

    feat: change api to zen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,41 @@
 "use client";
 import { Suspense, useState } from "react";
-import { QuoteBox } from "./components/ui/QuoteBox";
-import { categories } from "@/const/categories";
-import { Quote } from "@/types/Quote";
+import { ZenQuote } from "@/types/Quote";
 import { QuoteButton } from "./components/ui/SubmitButton";
-import { QuoteCategory } from "./components/ui/SelectionCategory";
 
 export default function Home() {
-  const [category, setCategory] = useState<string>("age");
-  const [quote, setQuote] = useState<Quote>({
-    quote: "",
-    author: "",
-    category: "",
+  const [quote, setQuote] = useState<ZenQuote>({
+    q: "",
+    a: "",
+    h: "",
   });
 
   const handleClick = async () => {
-    const res = await fetch(`/api/ninja?category=${category}`);
+    const res = await fetch(`/api/zen`);
     const data = await res.json();
     setQuote(data.data[0]);
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setCategory(e.target.value);
-  };
+  function createMarkup(c: string) {
+    return { __html: c };
+  }
 
   return (
     <main className="text-center">
-      <div className="mt-[100px] text-3xl mb-10">Get quotes for you day!</div>
+      <div className="mt-[100px] text-3xl mb-10">Get quotes for your day!</div>
       <Suspense fallback={<p>Loading Quote....</p>}>
-        <QuoteBox
-          quote={quote.quote}
-          author={quote.author}
-          category={quote.category}
-        />
+        <div>
+          {quote && <div dangerouslySetInnerHTML={createMarkup(quote.h)}></div>}
+        </div>
       </Suspense>
-
       <div className="flex flex-col gap-8 w-fit justify-center items-center m-auto mt-10">
-        <QuoteCategory handleChange={handleChange} categories={categories} />
         <QuoteButton handleClick={handleClick} buttonTitle="QuoteButton" />
+      </div>
+      <div className="mt-4">
+        Inspirational quotes provided by{" "}
+        <a href="https://zenquotes.io/" target="_blank">
+          ZenQuotes API
+        </a>
       </div>
     </main>
   );

58a1d1e7879ba73ea17e5838c3b80f38178a0d23
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-09-16 14:29:28 +0700

    feat: add x post btn

---
+++ src/app/components/ui/ShareTweet.tsx
@@ -0,0 +1,42 @@
+// import { ZenQuote } from "@/types/Quote";
+import React, { useEffect } from "react";
+import { IconParkSolidBigX } from "./IconParkSolidBigX";
+
+declare global {
+  interface Window {
+    twttr: {
+      widgets: {
+        load: () => void;
+      };
+    };
+  }
+}
+
+export default function ShareTweet({
+  author,
+  quote,
+}: {
+  author: string;
+  quote: string;
+}) {
+  useEffect(() => {
+    // Twitterウィジェットの再読み込み
+    if (window.twttr) {
+      window.twttr.widgets.load();
+    }
+  }, [author, quote]);
+  return (
+    <div className="border-white border-0 rounded-full hover:bg-gray-700">
+      <a
+        href={`https://twitter.com/share?text=${encodeURIComponent(
+          `${quote} - ${author}\n`
+        )}&hashtags=quote,for,today`}
+        target="_blank"
+        className="font-bold flex items-center px-4 gap-2"
+      >
+        <IconParkSolidBigX />
+        POST
+      </a>
+    </div>
+  );
+}
